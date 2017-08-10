d16579865b740ace61efbab169605c10c18d7c28
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-08-10 17:53:10 +0530

    check for wget and install it if not found
    Not every distro has wget installed and not everyone builds
    on Ubuntu. So let's check for it in advance and install it by
    identifying the distros package manager.
    
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- Lawnchair.sh
+++ Lawnchair.sh
@@ -5,12 +5,46 @@ url='https://labs.xda-developers.com/store/app/download/ch.deletescape.lawnchair
 #CUR_DIR=`pwd`
 DOWN_PATH='Lawnchair/'
 
-CHECK=`wget -q --tries=10 --timeout=20 --spider http://google.com`
+# Package manager check to install 'wget'
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/pacman.conf]=pacman
+osInfo[/etc/debian_version]=apt-get
+
+if which wget > /dev/null 2> /dev/null 
+then
+	echo "wget package found..."
+else
+	echo "wget package not found. Trying to install..."
+	for f in ${!osInfo[@]}
+	do
+		if [[ -f $f ]]; then
+			echo Package manager: ${osInfo[$f]}
+			TEMP=${osInfo[$f]}
+		fi
+	done
+
+	case "$TEMP" in
+		"yum")
+			`sudo yum install wget`
+		;;
+		"apt-get")
+			`sudo apt-get intall wget`
+		;;
+		"pacman")
+			`sudo pacman -S wget`
+		;;
+		*)
+			echo "Unknow Distribution! 'wget' couldn't be installed. Please install it manually"
+		;;
+	esac
+fi
+		
+CONNECTION_CHECK=`wget -q --tries=10 --timeout=20 --spider http://google.com`
 
 if [[ $? -eq 0 ]]; then
 	echo "Grabbing the latest version of Lawnchair"
-	DOWN=`wget -O $DOWN_PATH/Lawnchair.apk $url`
-	break
+	DOWN=`wget -q -O $DOWN_PATH/Lawnchair.apk $url`
 else
 	echo "Looks like you aren't connected to the Internet"
 	if [[ -f $DOWN_PATH/Lawnchair.apk ]] ; then
