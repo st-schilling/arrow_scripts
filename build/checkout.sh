a7f1db9d051b04c02dd273129a35401f20f35e30
Stefan Schilling <st-schilling@gmx.de>
2022-11-22 23:35:04 +0100

    Android AOSP Security Release 61 of November 2022: updated to new st-schilling authored kernel branch

--- build/checkout.sh
+++ build/checkout.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-python3 /mnt/arrowos/ajinasokan/bin/repo init --depth=1 -u https://st-schilling@github.com/st-schilling/android_manifest.git -b refs/tags/11.0.0_r60
-python3 /mnt/arrowos/ajinasokan/bin/repo init --depth=1 -u https://st-schilling@github.com/st-schilling/android_manifest.git -b feature/11.0.0_r58-update-build
+#export CHECKOUT_REFERENCE="refs/tags/11.0.0_r61"
+export CHECKOUT_REFERENCE="feature/11.0.0_r61-update-build"
+
+python3 /mnt/arrowos/ajinasokan/bin/repo init --depth=1 -u https://st-schilling@github.com/st-schilling/android_manifest.git -b $CHECKOUT_REFERENCE
 python3 /mnt/arrowos/ajinasokan/bin/repo sync  -f --force-sync --no-clone-bundle --no-tags -j4
-git clone -b arrow-11.0 --single-branch https://github.com/ArrowOS-Devices/android_kernel_motorola_sanders.git kernel/motorola/msm8953
+git clone -b arrow-11.0 --single-branch https://github.com/st-schilling/android_kernel_motorola_sanders.git  -b $CHECKOUT_REFERENCE kernel/motorola/msm8953
+#git clone -b arrow-11.0 --single-branch https://github.com/ArrowOS-Devices/android_kernel_motorola_sanders.git kernel/motorola/msm8953
 git clone -b arrow-11.0 --single-branch https://github.com/ArrowOS-Devices/android_device_motorola_sanders.git device/motorola/sanders
 git clone -b arrow-11.0 --single-branch https://github.com/ArrowOS-Devices/android_vendor_motorola_sanders.git vendor/motorola/sanders
 cp ../arrowos/arrows-11.0-sanders-ota-definition.json.source .
