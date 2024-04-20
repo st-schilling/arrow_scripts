9a0022a78cc9e1e8b1436d9a0a4a0c60cd1441b9
Stefan Schilling <st-schilling@gmx.de>
2024-04-20 16:36:19 +0200

    cloning latest rev. only - speeds up cloning a lot

--- build/checkout.sh
+++ build/checkout.sh
@@ -5,11 +5,11 @@ export CHECKOUT_REFERENCE="refs/tags/11.0.0_r76"
 
 python3 /mnt/arrowos/ajinasokan/bin/repo init --depth=1 -u https://st-schilling@github.com/st-schilling/android_manifest.git -b $CHECKOUT_REFERENCE
 python3 /mnt/arrowos/ajinasokan/bin/repo sync  -f --force-sync --no-clone-bundle --no-tags -j4
-git clone -b arrow-11.0 --single-branch https://github.com/st-schilling/android_kernel_motorola_sanders.git  -b $CHECKOUT_REFERENCE kernel/motorola/msm8953
-#git clone -b arrow-11.0 --single-branch https://github.com/ArrowOS-Devices/android_kernel_motorola_sanders.git kernel/motorola/msm8953
-git clone -b arrow-11.0 --single-branch https://github.com/ArrowOS-Devices/android_device_motorola_sanders.git device/motorola/sanders
-#git clone -b arrow-11.0 --single-branch https://github.com/st-schilling/android_device_motorola_sanders.git -b feature/fm_radio device/motorola/sanders
-git clone -b arrow-11.0 --single-branch https://github.com/ArrowOS-Devices/android_vendor_motorola_sanders.git vendor/motorola/sanders
+git clone -b arrow-11.0 --single-branch --depth 1 https://github.com/st-schilling/android_kernel_motorola_sanders.git  -b $CHECKOUT_REFERENCE kernel/motorola/msm8953
+#git clone -b arrow-11.0 --single-branch --depth 1 https://github.com/ArrowOS-Devices/android_kernel_motorola_sanders.git kernel/motorola/msm8953
+git clone -b arrow-11.0 --single-branch --depth 1 https://github.com/ArrowOS-Devices/android_device_motorola_sanders.git device/motorola/sanders
+#git clone -b arrow-11.0 --single-branch --depth 1 https://github.com/st-schilling/android_device_motorola_sanders.git -b feature/fm_radio device/motorola/sanders
+git clone -b arrow-11.0 --single-branch --depth 1 https://github.com/ArrowOS-Devices/android_vendor_motorola_sanders.git vendor/motorola/sanders
 cp ../arrowos/arrows-11.0-sanders-ota-definition.json.source .
 mkdir -p vendor/arrow/security/sanders
 cp ~/.android-certs/unprotected/* vendor/arrow/security/sanders/
